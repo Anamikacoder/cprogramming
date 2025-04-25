9eb5949b0642f8033598d1fd4cee90f733a35235
Anamika Singh <annu142017@gmail.com>
2025-04-25 14:09:53 +0530

    assignment 3and 2 completed

--- assignment2.2.c
+++ assignment2.2.c
@@ -1,30 +1,36 @@
 
 #include<stdio.h>
 int main(){
-char ch1,ch2,ch3;
+   char ch,ch1,ch2,ch3;
+   //answer no 6
+   printf("Enter the character");
+   scanf("%c",&ch);
+   printf("ASCII code of character %c is %d\n",ch,ch);
 
-   /*
    //answer no 7
-    printf("Enter the character");
-    scanf("%c",&ch1);
-    printf("ASCII code of character %c is %d",ch1,ch1);
+   int a;
+    
+   printf("Enter the ASCII code");
+   scanf("%d",&a);
+   printf("character of ASCII code %d is %c\n",a,a);
    
    //answer no. 8
-    printf("Enter the characters");
-    scanf("%c %c %c",&ch1,&ch2,&ch3);
-    printf("ASCII code of character %c %c %c is %d %d %d",ch1,ch2,ch3,ch1,ch2,ch3);
+   printf("Enter the characters");
+   scanf(" %c %c %c",&ch1,&ch2,&ch3);
+   printf("ASCII code of character%c:%d\n",ch1,ch1);
+   printf("ASCII code of character%c:%d\n",ch2,ch2);
+   printf("ASCII code of character%c:%d\n",ch3,ch3);
     
-     
-    //answer no. 9
-    int d,m,y;
-    printf("Enter the date in DD/MM/YY formate");
-    scanf("%d/%d/%d",&d,&m,&y);
-    printf("Date - %d  Month - %d Year - %d",d,m,y);
-    */
+   //answer no. 9
+   int d,m,y;
+   printf("Enter the date in DD/MM/YY formate");
+   scanf("%d/%d/%d",&d,&m,&y);
+   printf("Date - %d  Month - %d Year - %d\n",d,m,y);
+   
    //answer no. 10
-   int h,m;
+   int h,mi;
    printf("Enter the time(HH:MM)");
-   scanf("%d:%d",&h,&m);
-   printf("%d Hours %d Minutes",h,m);
+   scanf("%d:%d",&h,&mi);
+   printf("%d Hours %d Minutes",h,mi);
    return 0;
 }
--- assignment2.c
+++ assignment2.c
@@ -8,21 +8,25 @@ int main(){
 	avg=(a+b+c)/3.0; 
 	 //if we perform same type of data then result also is sae type
 	printf("Average of numbers %d,%d and %d is %f \n",a,b,c,avg);
-		//answer2
+    
+	//answer2
 	printf("Enter the radius of the circle ");
 	scanf("%d",&a);
 	cir=2*3.14*a;
 	printf("Circumference of circle is %f having radius %d \n",cir,a);
+
     //answer3
     printf("Enter the value of Ammount Rate and Time");
     scanf("%d %d %d",&a, &b,&c);
     si=(a*b*c)/100.0;
     printf("Simple interest of amount %d with rate %d in time %d is %f \n",a,b,c,si);
+
     //answer4
     printf("Enter length,width and hieght of cuboid");
     scanf("%d %d %d",&a,&b,&c);
     v=a*b*c;
     printf("volume of cuboid is %d having length %d, width %d and hieght %d \n",v,a,b,c);
+
     //answer5
     float cp,sp,profit;
     printf("Enter cost price and selling price of bnanas");
